5790b0fa9a0d69dbd066729272430d8a45d75159
ValentynaVK <valia2526.vk@gmail.com>
2025-04-29 11:49:05 +0300

    finish

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -3,12 +3,14 @@ import css from "./ImageGallery.module.css";
 
 export default function ImageGallery({ items, onImageClick }) {
   return (
-    <ul className={css.list}>
-      {items.map((item) => (
-        <li className={css.item} key={item.id}>
-          <ImageCard image={item} onClick={() => onImageClick(item)} />
-        </li>
-      ))}
-    </ul>
+    <div className={css.galleryContainer}>
+      <ul className={css.list}>
+        {items.map((item) => (
+          <li className={css.item} key={item.id}>
+            <ImageCard image={item} onClick={() => onImageClick(item)} />
+          </li>
+        ))}
+      </ul>
+    </div>
   );
 }
